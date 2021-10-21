2c09fd7ddd1ee991d83c0840f2c665e2b7621221
Ajay Titus <ajaytitus1386@gmail.com>
2021-10-21 16:49:12 +0530

    chore(app) : config files

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,29 @@
 module.exports = {
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  mode : 'jit',
+  purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'noto-sans' : ['"Noto Sans"', 'sans-serif'],
+        'noto-serif' : ['"Noto Serif"', 'serif']
+      },
+      colors:{
+        primary: '#324376',
+        secondary: '',
+        bg: '#F0F0F0',
+        hover: '#F0F0F0',
+        block: '#FFFFFF',
+      },
+      spacing:{
+        'expanded' : '28.25rem', //sidebar
+        'folded': '8.75rem'
+      }
+    },
+
   },
   variants: {
     extend: {},
   },
   plugins: [],
-};
+}
