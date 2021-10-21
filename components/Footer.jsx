0261a9b7d1e95f261507e676ec9e3a5f698a1724
Ajay Titus <ajaytitus1386@gmail.com>
2021-10-21 16:36:23 +0530

    feat(layout) : horizontal bottom footer

---
+++ components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div className="flex flex-row justify-around items-center h-24">
+            <h1 className="text-xl font-bold">Copyright 2021</h1>
+        </div>
+    )
+}
+
+export default Footer
