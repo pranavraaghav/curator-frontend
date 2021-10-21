af73ce88d114dda4c6da29401d08e6283b6e224f
Ajay Titus <ajaytitus1386@gmail.com>
2021-10-21 16:35:47 +0530

    feat(Layout) : horizontal top navbar

---
+++ components/NavBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NavBar = () => {
+    return (
+        <div className="sticky top-0 flex flex-row items-center justify-around w-screen h-24 bg-block">
+            <h1 className="text-3xl font-noto-serif">Curator</h1>
+        </div>
+    )
+}
+
+export default NavBar
