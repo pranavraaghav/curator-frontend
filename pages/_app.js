8c39be783f583b83224cb5d04e251281eca82295
Ajay Titus <ajaytitus1386@gmail.com>
2021-10-21 16:27:56 +0530

    feat(app) : cdn links icons and temp routing

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,23 @@
-import "tailwindcss/tailwind.css";
+import Head from 'next/head';
+
+import Layout from '../components/Layout'
+import '../styles/globals.css'
+import 'tailwindcss/tailwind.css';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+    <Head>
+        <title>Curator</title>
+        <link href="https://cdn.lineicons.com/3.0/lineicons.css" rel="stylesheet" /> 
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta2/css/all.min.css" integrity="sha512-YWzhKL2whUzgiheMoBFwW8CKV4qpHQAEuvilg9FAn5VJUDwKZZxkJNuGM4XkWuk94WCrrwslk8yWNGmY1EduTA==" crossOrigin="anonymous" referrerPolicy="no-referrer" />
+    </Head>
+    <Layout>
+      <Component {...pageProps} />
+    </Layout>
+    </>
+  )
 }
 
-export default MyApp;
+export default MyApp
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,34 @@
-import Head from "next/head";
-import Image from "next/image";
+import React from "react"
+import Link from 'next/link'
+import { useState, useEffect } from "react"
+import "@material-tailwind/react/tailwind.css";
+import Head from "next/dist/shared/lib/head"
+
 
 export default function Home() {
+
+  // useEffect(() => {
+  //   const apiEndpoint = '';
+  //   const res =  fetch(apiEndpoint);
+  //   const data =  res.json();
+
+  //   setCurations(data);
+
+  // }, [])
+
   return (
-    <div className="bg-red-500 w-screen h-screen text-white text-center">
-      S U P R E M E
+    <>
+    <Head>
+      <title>Home</title>
+    </Head>
+    <div className="w-screen h-screen fcc bg-bg">
+        <div className="w-1/2 p-10 space-y-4 text-xl border-2 border-gray-300 rounded-lg bg-block fcc text-primary">
+            <Link href="/dashboard/a_random_user">Go Here For Now</Link>
+            <Link href="/dashboard/a_random_user/new_curation">A Big ol' Form</Link>
+            <Link href="/login">Welcome Back Buddy</Link>
+            <Link href="/register">First Timer huh?</Link>
+        </div>
     </div>
-  );
+    </>
+  )
 }
