4e94af76ec40ff78d9eb8e1e21efce59641fb5ee
Ajay Titus <ajaytitus1386@gmail.com>
2021-10-21 16:33:18 +0530

    feat(dashboard) : add user dashboard

---
+++ pages/dashboard/[username]/new_curation.jsx
@@ -0,0 +1,10 @@
+
+function NewCuration() {
+    return (
+        <div>
+            <h1>Normally, you'd see a nice looking form here...</h1>
+        </div>
+    );
+}
+
+export default NewCuration;
