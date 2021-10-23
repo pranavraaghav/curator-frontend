8a0ab4878003f3aab7c28c82fefccbfd15274ebb
Ajay Titus <ajaytitus1386@gmail.com>
2021-10-23 12:06:01 +0530

    feat(dashboard) : add loading overlay

---
+++ components/LoadingOverlay.jsx
@@ -0,0 +1,11 @@
+function LoadingOverlay( { children } ) {
+    return (
+        <div className="fixed w-full h-full fcc backdrop-filter backdrop-blur-sm">
+            <div className="p-4 bg-gray-100 rounded-lg bg-opacity-80 fcc ">
+                {children}
+            </div>
+        </div>
+    );
+}
+
+export default LoadingOverlay;
