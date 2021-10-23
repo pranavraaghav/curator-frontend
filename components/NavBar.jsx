47f23fc25fa3a4562a6ecf9c74e7e27ec1003629
Ajay Titus <ajaytitus1386@gmail.com>
2021-10-23 12:09:57 +0530

    fix(app) : width issue fixed

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const NavBar = () => {
     return (
-        <div className="sticky top-0 flex flex-row items-center justify-around w-screen h-24 bg-block">
+        <div className="sticky top-0 flex flex-row items-center justify-around w-full h-24 bg-block">
             <h1 className="text-3xl font-noto-serif">Curator</h1>
         </div>
     )
