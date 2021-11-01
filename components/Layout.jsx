afba7284a36d736816eaf5e7f6497ba5903bd4de
Ajay Titus <ajaytitus1386@gmail.com>
2021-11-01 13:46:29 +0530

    feat : add sidebar prototype

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,15 +1,20 @@
+import { useRouter } from 'next/dist/client/router'
 import React from 'react'
+import Head from 'next/head'
+import { useState } from 'react'
+
 import Footer from './Footer'
 import NavBar from './NavBar'
-import Head from 'next/head'
-import Image from 'next/image'
 import Sidebar from './Sidebar'
-import { useRouter } from 'next/dist/client/router'
 
 // import * as layout from '../styles/components/Layout.module.css'
 
 const Layout = ({children}) => {
     const { pathname } = useRouter()
+
+    const [sidebarActive, setSidebarActive] = useState(true)
+
+    const toggleSidebar = () => {setSidebarActive(!sidebarActive)}
 
     // Hides Sidebar if the current route shouldn't have it like the login or register pages
     const authRoutes = ['/login','/register','/']
@@ -24,15 +29,13 @@ const Layout = ({children}) => {
                 <link rel="icon" href="/favicon.ico" />
                 <base target="_blank"></base>
              </Head>
-            <NavBar />
-            <div className="flex flex-row items-center justify-center">
-                <div className="flex flex-col ">
+            <NavBar hideSidebar={hideSidebar} toggleSidebar={toggleSidebar} sidebarActive={sidebarActive}/>
+            <div className="frc">
+                <div className="flex flex-col w-full">
                     {children}
                     <Footer />
                 </div>
-                {/* <div className="relative">
-                    { !hideSidebar && <Sidebar />}
-                </div> */}
+                { !hideSidebar && <Sidebar toggleSidebar={toggleSidebar} sidebarActive={sidebarActive}/>}
             </div>
         </div>
     )
--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,22 +1,79 @@
 import { useState } from "react";
+import { motion } from 'framer-motion'
 
-function Sidebar() {
-    const [sidebarActive, setSidebarActive] = useState(true)
-
-    const toggleSidebar = () => {setSidebarActive(!sidebarActive)}
+function Sidebar({toggleSidebar, sidebarActive}) {
 
     return (
-        <div className="sticky top-0 flex flex-col items-start justify-start h-full bg-red-500">
-            <div className={`items-start transition-all duration-500 ease-in-out ${sidebarActive ? "w-expanded" : "w-folded "}`}>
-                <div className="text-9xl">
-                    {sidebarActive ? (<strong>Curator</strong>) : (<strong>C</strong>)}
+            <aside className={`fixed right-0 drop-shadow-xl flex flex-col bg-sidebar rounded-l-3xl h-full items-center justify-between top-0 transition-all duration-700 ease-in-out overflow-hidden ${sidebarActive ? "w-72 lg:w-expanded" : "w-0 lg:w-folded "}`}>
+                <div className="w-full fcc"> 
+                    <div className="flex flex-row items-center justify-between w-full px-8 py-4 mx-4">
+                        <div onClick={toggleSidebar} className="right-0 cursor-pointer">
+                            <motion.span animate={sidebarActive ? {rotate:180,} : {rotate:0}} className="text-6xl rounded-full text-coal material-icons-round bg-hover">
+                                arrow_left
+                            </motion.span>
+                        </div>
+                        <div className="text-5xl font-extrabold text-primary font-noto-sans">
+                            {sidebarActive ? (<strong>Curator</strong>) : (<strong></strong>)}
+                        </div>
+                    </div>
+                    <HorizBar />
+                    <div className={`flex flex-col w-full px-8 py-4 mx-4 space-y-4 ${sidebarActive ? "items-start" : "items-center"}`}>
+                        <h1 className="text-lg font-semibold text-primary">DASHBOARD</h1>
+                        <ul className="flex flex-col items-start space-y-2 ">
+                            <DashListItem icon={"account_circle"} label={sidebarActive ? "Profile" : null} />
+                            <DashListItem icon={"list_alt"} label={sidebarActive ? "Your Curations" : null} />
+                            <DashListItem icon={"add"} label={sidebarActive ? "New Curation" : null} />
+                        </ul>
+                    </div>
+                    <HorizBar />
+                    <div className={`flex flex-col w-full px-8 py-4 mx-4 space-y-4 ${sidebarActive ? "items-start" : "items-center"}`}>
+                        <h1 className="text-lg font-semibold text-primary">CURATIONS</h1>
+                        <ul className="flex flex-col items-start space-y-2 ">
+                            
+                        </ul>
+                    </div>
+                    <HorizBar />
                 </div>
-                <div onClick={toggleSidebar} className="cursor-pointer">
-                    <span className="material-icons">chevron_right</span>
+                <div className="flex items-center justify-center w-full h-32 frc bg-whisper">
+                    <div className="flex flex-row items-center justify-around w-full">
+                        <div className="frc">
+                            <div className="w-12 h-12 rounded-full bg-coal"></div>
+                            {
+                                sidebarActive &&
+                                <div className="flex flex-col items-start mx-2 text-primary font-noto-sans">
+                                    <h2 className="font-bold">Username</h2>
+                                    <h3 className="font-light">user@email.com</h3>
+                                </div>
+                            }
+                        </div>
+                        {
+                            sidebarActive &&
+                            <span className="text-3xl material-icons text-coal">settings</span>
+                        }
+                    </div>
                 </div>
-            </div>
-        </div>
+
+            </aside>
     );
+}
+
+function HorizBar() {
+
+    return (
+        <div className="w-full h-0.5 m-0 opacity-50 bg-ash">
+
+        </div>
+    )
+}
+
+function DashListItem( { icon, label, sidebarActive } ) {
+
+    return (
+        <li className="px-2 space-x-4 rounded-lg frc hover:bg-hover">
+            <span className="material-icons-round text-primary">{icon}</span> 
+            {label && <h2 className="text-lg font-normal tracking-wide text-primary">{label}</h2>}
+        </li>   
+    )
 }
 
 export async function getServerSideProps(ctx){
