74e594722eb7676a103c9740432bafd055ec4d88
Ajay Titus <ajaytitus1386@gmail.com>
2021-11-15 16:51:42 +0530

    feat: add animation variant

--- components/global/cardVariants.js
+++ components/global/cardVariants.js
@@ -1,26 +1,33 @@
 const cardVariants = {
-    // hidden : {
+  // hidden : {
 
-    // },
-    // initial :{
-        
-    // },
-    visible : {
-        transition : {
-            type : "tween",
-            ease: "easeInOut",
-            duration : 2,
-        },
+  // },
+  initial: {
+    x: 0,
+    y: 0,
+    opacity: 1,
+  },
+  visible: {
+    x: 0,
+    y: 0,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      ease: "easeInOut",
+      duration: 0.5,
     },
-    hover : {
-        boxShadow: "2px 2px 8px 10px rgba(0,0,0,0.2)",
-    }
+  },
+  initialUp: {
+    opacity: 0.3,
+    y: -20,
+  },
+  hover: {
+    boxShadow: "2px 2px 8px 10px rgba(0,0,0,0.2)",
+  },
 }
 
 const cardIconVariants = {
-    hover : {
-        
-    }
+  hover: {},
 }
 
-export { cardVariants };
+export { cardVariants }
